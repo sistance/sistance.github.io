754cb8d3f18178aef4615b3ef6418b37515b533a
sistance <sistance@yahoo.com>
2020-05-23 01:13:20 -0500

    Delete scripts.js

--- experiments/dockbuilder/scripts.js
+++
@@ -1,5 +0,0 @@
-var ele = document.getElementById('download_link');
-ele.addEventListener('click',function(e){
-	var canvas = document.getElementById('dock_builder');
-	ele.href = canvas.toDataURL();
-});
